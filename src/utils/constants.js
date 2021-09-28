3a31e3392c581fed4b621005745f79c7854a6860
Иван Галайдо <75268958+Ivan-Galaido@users.noreply.github.com>
2021-09-28 10:39:57 +0300

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,117 @@
-export const PROPS_PROFILE = {
+import card1 from "../images/card1.jpg";
+import card2 from "../images/card2.jpg";
+import card3 from "../images/card3.jpg";
+import card4 from "../images/card4.jpg";
+
+export const cards = [
+  {
+    id: 1,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card1,
+    },
+    saved: false,
+  },
+  {
+    id: 2,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card2,
+    },
+    saved: false,
+  },
+  {
+    id: 3,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card3,
+    },
+    saved: true,
+  },
+  {
+    id: 4,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: true,
+  },
+  {
+    id: 5,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: false,
+  },
+  {
+    id: 6,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: false,
+  },
+  {
+    id: 7,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: true,
+  },
+  {
+    id: 8,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: false,
+  },
+  {
+    id: 9,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: false,
+  },
+  {
+    id: 10,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: false,
+  },
+  {
+    id: 11,
+    nameRU: "В погоне за Бенкси",
+    duration: 27 * 60,
+    image: {
+      url: card4,
+    },
+    saved: false,
+  },
+];
+    /* export const PROPS_PROFILE = {
   inputsList: [
     { name: "name", label: "Имя", type: "text", maxLength: 30 },
     { name: "email", label: "Почта", type: "text" },
   ],
   defaultValues: {
     name: "",
-    email: "",
+    email: "", 
   },
   validators: {
     name: {
@@ -43,7 +149,7 @@ export const PROPS_PROFILE = {
       },
     },
   },
-};
+};*/
 
 export const PROPS_LOGIN = {
   inputsList: [
